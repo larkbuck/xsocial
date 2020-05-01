470a6cbb67174a2a110e2a71700938ff469d72ca
L A R K <lark.buckingham@sjsu.edu>
2020-04-30 18:12:15 -0700

    getting funky

--- tuts/1-wild-chat/js/sketch.js
+++ tuts/1-wild-chat/js/sketch.js
@@ -81,9 +81,10 @@ function displayPastChats() {
     let p = createP(fbDataArray[i].message);
     p.position(random(windowWidth), random(windowHeight)); // gives them random position
     p.class('message');
-    p.style('background-color', `hsl(${(i * 5) % 300}, 80%, 50%)`);
+    p.style('background-color', `hsl(${(i * 5) % 300}, 80%, 50%)`); // changed color mode to HSL 
     // p.style('background-color', `rgb(${200 - i / length * 100}, ${i / length * 50}, ${i / length * 255})`);
-    p.style('opacity', i / length);
+    let opacity = map(i / length, 0, 1, 0, .9);
+    p.style('opacity', opacity);
     p.parent('messagesDiv');
   }
 
